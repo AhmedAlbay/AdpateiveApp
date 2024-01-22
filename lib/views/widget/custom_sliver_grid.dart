fbe45b9aab05590cbe945bc23d338b58a04f28dd
Your Name <you@example.com>
2024-01-22 15:24:06 +0200

    refactor

--- lib/views/widget/custom_sliver_grid.dart
+++ lib/views/widget/custom_sliver_grid.dart
@@ -1,3 +1,4 @@
+import 'package:adpateive_app/views/widget/custom_item.dart';
 import 'package:flutter/material.dart';
 
 class CustomSliverGrid extends StatelessWidget {
@@ -10,12 +11,7 @@ class CustomSliverGrid extends StatelessWidget {
             mainAxisSpacing: 16, crossAxisSpacing: 16, crossAxisCount: 2),
         itemCount: 4,
         itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              color: const Color(0xffB4B4B4),
-              borderRadius: BorderRadius.circular(8),
-            ),
-          );
+          return const CustomItem();
         });
   }
 }
