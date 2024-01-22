64aef9d12076c4021e8a8b97fb31f1e92ed21471
Your Name <you@example.com>
2024-01-22 15:43:32 +0200

    make a drawer

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,21 +1,34 @@
+import 'package:adpateive_app/views/widget/custom_drawer.dart';
 import 'package:adpateive_app/views/widget/home_view_body.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      drawer: const CustomDrawer(),
       backgroundColor: const Color(0xffDBDBDB),
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        leading: const Icon(
-          Icons.menu,
-          color: Colors.white,
-        ),
-
-      ),
+          backgroundColor: Colors.black,
+          leading: IconButton(
+            onPressed: () {
+              scaffoldKey.currentState!.openDrawer();
+            },
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+          )),
       body: const HomeViewBody(),
     );
   }
